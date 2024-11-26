1c6a978798b43b09bc03ce99bad26d09102c39d6
Calin <acalin729@gmail.com>
2024-11-26 15:02:08 +0200

    backend setup

---
+++ backend/src/test/java/com/Matchday_Manager/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Matchday_Manager.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
